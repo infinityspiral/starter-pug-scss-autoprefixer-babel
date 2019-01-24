4adf876dfcfd281418d76d1d1ad151754495af43
CN58090 <NortonChristopherP@johndeere.com>
2019-01-24 09:59:33 -0600

    feat: auto set up polyfills

---
+++ dist/js/app.js
@@ -0,0 +1,2 @@
+"use strict";var currYear=new Date().getFullYear();console.log("current year is ".concat(currYear));var numbers=[1,2,3];numbers.map(function(a){return a+1}),console.log(numbers);
+//# sourceMappingURL=app.js.map
--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,7 @@
 const currYear = new Date().getFullYear();
 console.log(`current year is ${currYear}`);
+const numbers = [1,2,3];
+numbers.map(item => {
+    return item + 1;
+})
+console.log(numbers)
